d8b07670010c6ae693f665371d2414c4a092ed82
ghydq <ghydqme@gmail.com>
2022-07-28 16:10:52 +0300

    ssssss

--- discord/src/init-oauth.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$discord_url = "https://discord.com/api/oauth2/authorize?client_id=1000355327474151544&redirect_uri=https%3A%2F%2Fpluses.gq%2Fdiscord%2Fsrc%2Fdashboard.php&response_type=code&scope=identify";
-header("Location: $discord_url");
-exit();
-
-?>
