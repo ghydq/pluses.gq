b5bd89e3ca900dcbffe873bc144cfb4d54765ec0
ghydq <ghydqme@gmail.com>
2022-07-28 10:48:03 +0300

    ssssssss

--- discord/discord.js
+++ discord/discord.js
@@ -3,3 +3,16 @@ const express = require('express');
 
 
 const app = express();
+
+app.use('',express.static(path.join(__dirname, 'public')));
+
+app.get('/', (request, response) => {
+	return response.sendFile('index.html', { root: '.' });
+});
+
+app.get('/auth/discord', (request, response) => {
+	return response.sendFile('dashboard.html', { root: '.' });
+});
+
+const port = '53134';
+app.listen(port, () => console.log(`App listening at https://pluses.gq:${port}`));
